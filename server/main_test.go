373b98b4c0bb2cdbcd1f86f9c860b38a910eee18
blinky-z <dm.blink19@gmail.com>
2019-01-16 21:32:52 +0300

    Update tests
    Add tests for GET, POST methods

--- server/main_test.go
+++ server/main_test.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	. "../models"
+	. "./models"
 	"bytes"
 	"encoding/json"
+	"github.com/spf13/cast"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"testing"
+	"time"
 )
+
+func TestRunServer(t *testing.T) {
+	go RunServer("testConfig", ".")
+	time.Sleep(3 * time.Second)
+	Db.Exec("DELETE from testdb.public.posts")
+	Db.Exec("ALTER SEQUENCE posts_id_seq RESTART WITH 1")
+}
 
 func TestCreatePost(t *testing.T) {
 	message := map[string]interface{}{
@@ -22,16 +31,22 @@ func TestCreatePost(t *testing.T) {
 	}
 
 	resp, err := http.Post("http://localhost:8080/posts", "application/json", bytes.NewBuffer(encodedMessage))
-	defer resp.Body.Close()
 	if err != nil {
 		log.Fatalf("Can not send request. Error: %s", err)
 	}
+	if resp.StatusCode != http.StatusCreated {
+		log.Fatal(resp.Body)
+	}
+	defer resp.Body.Close()
 }
 
 func TestGetCertainPost(t *testing.T) {
 	resp, err := http.Get("http://localhost:8080/posts/1")
 	if err != nil {
 		log.Fatalf("Can not send request. Error: %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(resp.Body)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -46,17 +61,56 @@ func TestGetCertainPost(t *testing.T) {
 	}
 
 	var properPost Post
-	err = json.Unmarshal(body, &encodedMessage)
-	if err != nil {
-		log.Fatalf("Error decoding proper body. Error: %s", err)
-	}
+	properPost.ID = "1"
+	properPost.Title = "Title1"
+	properPost.Content = "Content1 Content2 Content3"
 
-	if receivedPost != properPost {
+	if receivedPost.ID != properPost.ID || receivedPost.Title != properPost.Title ||
+		receivedPost.Content != properPost.Content {
 		log.Fatalf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
 			receivedPost, properPost)
 	}
 }
 
-func main() {
-	//RunServer("testConfig", "testPath")
+func TestGetPosts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		message := map[string]interface{}{
+			"title":   "Title" + cast.ToString(i),
+			"content": "Content" + cast.ToString(i),
+		}
+
+		encodedMessage, _ := json.Marshal(message)
+		http.Post("http://localhost:8080/posts", "application/json", bytes.NewBuffer(encodedMessage))
+	}
+
+	resp, err := http.Get("http://localhost:8080/posts")
+	if err != nil {
+		log.Fatalf("Can not send request. Error: %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(resp.Body)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading received body. Error: %s", err)
+	}
+
+	var receivedPosts []Post
+	err = json.Unmarshal(body, &receivedPosts)
+	if err != nil {
+		log.Fatalf("Error decoding received body. Error: %s", err)
+	}
+
+	for i, receivedPost := range receivedPosts[1:] {
+		curPostIndex := cast.ToString(i)
+		databaseOffset := cast.ToString(i + 2)
+		properPost := Post{ID: databaseOffset, Title: "Title" + curPostIndex, Content: "Content" + curPostIndex}
+
+		if receivedPost.ID != properPost.ID || receivedPost.Title != properPost.Title ||
+			receivedPost.Content != properPost.Content {
+			log.Fatalf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
+				receivedPost, properPost)
+		}
+	}
 }
