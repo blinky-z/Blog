a5dc118968640b9163927cc18f37a3673aa45497
blinky-z <dm.blink19@gmail.com>
2019-01-21 00:16:50 +0300

    Add new tests

--- server/main_test.go
+++ server/main_test.go
@@ -3,112 +3,290 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	. "github.com/Blog/server/models"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"testing"
-	"time"
 )
+
+var (
+	workingPost Post
+	client      = &http.Client{}
+)
+
+type Response struct {
+	Error string
+	Body  Post
+}
+
+func encodeMessage(message interface{}) []byte {
+	encodedMessage, err := json.Marshal(message)
+	if err != nil {
+		panic(fmt.Sprintf("Error encoding message. Error: %s", err))
+	}
+
+	return encodedMessage
+}
+
+func decodeMessage(responseBody io.ReadCloser, resp *Response) {
+	err := json.NewDecoder(responseBody).Decode(resp)
+	if err != nil {
+		panic(fmt.Sprintf("Error decoding received body. Error: %s", err))
+	}
+}
 
 func TestRunServer(t *testing.T) {
 	go RunServer("testConfig", ".")
-	time.Sleep(3 * time.Second)
-	Db.Exec("DELETE from testdb.public.posts")
-	Db.Exec("ALTER SEQUENCE posts_id_seq RESTART WITH 1")
+	for {
+		resp, err := http.Get("http://" + Address + "/hc")
+		if err == nil && resp.StatusCode == http.StatusOK {
+			break
+		}
+	}
+}
+
+func sendMessage(method, address string, message interface{}) *http.Response {
+	var response *http.Response
+
+	switch method {
+	case "GET":
+		request, err := http.NewRequest("GET", address, strings.NewReader(""))
+		if err != nil {
+			panic(fmt.Sprintf("Can not create request. Error: %s", err))
+		}
+
+		response, err = client.Do(request)
+		if err != nil {
+			panic(fmt.Sprintf("Can not send request. Error: %s", err))
+		}
+	case "POST":
+		encodedMessage := encodeMessage(message)
+
+		request, err := http.NewRequest("POST", address, bytes.NewReader(encodedMessage))
+		request.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			panic(fmt.Sprintf("Can not create request. Error: %s", err))
+		}
+
+		response, err = client.Do(request)
+		if err != nil {
+			panic(fmt.Sprintf("Can not send request. Error: %s", err))
+		}
+	case "PUT":
+		encodedMessage := encodeMessage(message)
+
+		request, err := http.NewRequest("PUT", address, bytes.NewReader(encodedMessage))
+		request.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			panic(fmt.Sprintf("Can not create request. Error: %s", err))
+		}
+
+		response, err = client.Do(request)
+		if err != nil {
+			panic(fmt.Sprintf("Can not send request. Error: %s", err))
+		}
+	case "DELETE":
+		request, err := http.NewRequest("DELETE", address, strings.NewReader(""))
+		if err != nil {
+			panic(fmt.Sprintf("Can not create request. Error: %s", err))
+		}
+
+		response, err = client.Do(request)
+		if err != nil {
+			panic(fmt.Sprintf("Can not send request. Error: %s", err))
+		}
+	}
+
+	return response
 }
 
 func TestCreatePost(t *testing.T) {
+	var response Response
+
 	message := map[string]interface{}{
 		"title":   "Title1",
 		"content": "Content1 Content2 Content3",
 	}
 
-	encodedMessage, err := json.Marshal(message)
-	if err != nil {
-		log.Fatalf("Can not encode message. Error: %s", err)
+	resp := sendMessage("POST", "http://"+Address+"/posts", message)
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusCreated {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
 	}
 
-	resp, err := http.Post("http://localhost:8080/posts", "application/json", bytes.NewBuffer(encodedMessage))
-	if err != nil {
-		log.Fatalf("Can not send request. Error: %s", err)
+	workingPost = response.Body
+
+	log.Printf("Created post id: %s", workingPost.ID)
+}
+
+func TestCreatePostWithInvalidTitle(t *testing.T) {
+	var response Response
+
+	message := map[string]interface{}{
+		"title":   strings.Repeat("a", 130),
+		"content": "Content1 Content2 Content3",
 	}
-	if resp.StatusCode != http.StatusCreated {
-		log.Fatal(resp.Body)
-	}
+
+	resp := sendMessage("POST", "http://"+Address+"/posts", message)
+	decodeMessage(resp.Body, &response)
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
 }
 
 func TestGetCertainPost(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/posts/1")
-	if err != nil {
-		log.Fatalf("Can not send request. Error: %s", err)
-	}
+	var response Response
+
+	resp := sendMessage("GET", "http://"+Address+"/posts/"+workingPost.ID, "")
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.Body)
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading received body. Error: %s", err)
+	receivedPost := response.Body
+
+	if receivedPost != workingPost {
+		t.Errorf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
+			receivedPost, workingPost)
+	}
+}
+
+func TestGetNonexistentPost(t *testing.T) {
+	var response Response
+
+	resp := sendMessage("GET", "http://"+Address+"/posts/-1", "")
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
+}
+
+func TestUpdatePost(t *testing.T) {
+	var response Response
+
+	newPost := workingPost
+	newPost.Title = "newTitle"
+	newPost.Content = "NewContent"
+
+	resp := sendMessage("PUT", "http://"+Address+"/posts/"+workingPost.ID, newPost)
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
 	}
 
-	var receivedPost Post
-	err = json.Unmarshal(body, &receivedPost)
-	if err != nil {
-		log.Fatalf("Error decoding received body. Error: %s", err)
+	updatedPost := response.Body
+	if updatedPost != newPost {
+		t.Errorf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
+			updatedPost, workingPost)
 	}
 
-	var properPost Post
-	properPost.ID = "1"
-	properPost.Title = "Title1"
-	properPost.Content = "Content1 Content2 Content3"
+	workingPost = updatedPost
+}
 
-	if receivedPost.ID != properPost.ID || receivedPost.Title != properPost.Title ||
-		receivedPost.Content != properPost.Content {
-		log.Fatalf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
-			receivedPost, properPost)
+func TestUpdatePostWithInvalidTitle(t *testing.T) {
+	var response Response
+
+	message := map[string]interface{}{
+		"title":   strings.Repeat("a", 130),
+		"content": "Content1 Content2 Content3",
+	}
+
+	resp := sendMessage("PUT", "http://"+Address+"/posts/"+workingPost.ID, message)
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
+}
+
+func TestUpdateNonexistentPost(t *testing.T) {
+	var response Response
+
+	message := map[string]interface{}{
+		"title":   "TITLE2",
+		"content": "Content1 Content2 Content3",
+	}
+
+	resp := sendMessage("PUT", "http://"+Address+"/posts/-1", message)
+	decodeMessage(resp.Body, &response)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
+}
+
+func TestDeletePost(t *testing.T) {
+	var response Response
+
+	resp := sendMessage("DELETE", "http://"+Address+"/posts/"+workingPost.ID, "")
+	decodeMessage(resp.Body, &response)
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
+	resp.Body.Close()
+
+	resp = sendMessage("GET", "http://"+Address+"/posts/"+workingPost.ID, "")
+	defer resp.Body.Close()
+	decodeMessage(resp.Body, &response)
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
+	}
+}
+
+func TestDeleteNonexistentPost(t *testing.T) {
+	var response Response
+
+	resp := sendMessage("DELETE", "http://"+Address+"/posts/-1", "")
+	defer resp.Body.Close()
+	decodeMessage(resp.Body, &response)
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("Error %d. Error message: %s", resp.StatusCode, response.Error)
 	}
 }
 
 func TestGetPosts(t *testing.T) {
-	for i := 0; i < 10; i++ {
+	var workingPosts []Post
+
+	testPostsNumber := 10
+
+	for i := 0; i < testPostsNumber; i++ {
 		message := map[string]interface{}{
 			"title":   "Title" + strconv.Itoa(i),
 			"content": "Content" + strconv.Itoa(i),
 		}
 
-		encodedMessage, _ := json.Marshal(message)
-		http.Post("http://localhost:8080/posts", "application/json", bytes.NewBuffer(encodedMessage))
+		var response Response
+		resp := sendMessage("POST", "http://"+Address+"/posts", message)
+		decodeMessage(resp.Body, &response)
+
+		workingPosts = append(workingPosts, response.Body)
 	}
 
-	resp, err := http.Get("http://localhost:8080/posts")
-	if err != nil {
-		log.Fatalf("Can not send request. Error: %s", err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.Body)
+	type responseAllPosts struct {
+		Error string
+		Body  []Post
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading received body. Error: %s", err)
-	}
+	resp := sendMessage("GET", "http://"+Address+"/posts", "")
 
-	var receivedPosts []Post
-	err = json.Unmarshal(body, &receivedPosts)
+	var response responseAllPosts
+	err := json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
 		log.Fatalf("Error decoding received body. Error: %s", err)
 	}
 
-	for i, receivedPost := range receivedPosts[1:] {
-		curPostIndex := strconv.Itoa(i)
-		databaseOffset := strconv.Itoa(i + 2)
-		properPost := Post{ID: databaseOffset, Title: "Title" + curPostIndex, Content: "Content" + curPostIndex}
-
-		if receivedPost.ID != properPost.ID || receivedPost.Title != properPost.Title ||
-			receivedPost.Content != properPost.Content {
+	for i, receivedPost := range response.Body[len(response.Body)-testPostsNumber:] {
+		properPost := workingPosts[i]
+		if receivedPost != properPost {
 			log.Fatalf("Received post does not match proper post\nReceived post: %v\n Proper post: %v",
 				receivedPost, properPost)
 		}
