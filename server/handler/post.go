f3d2ea890dbda260e46b89ceab21206faacf240e
blinky-z <dm.blink19@gmail.com>
2019-01-21 00:16:26 +0300

    Add new handlers

--- server/handler/post.go
+++ server/handler/post.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"github.com/Blog/server/models"
 	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
@@ -17,19 +16,29 @@ var (
 	Db *sql.DB
 )
 
+type Response struct {
+	Error postErrorCode `json:"error"`
+	Body  interface{}   `json:"body"`
+}
+
+type postErrorCode string
+
+const (
+	TechnicalError postErrorCode = "TECHNICAL_ERROR"
+	InvalidTitle   postErrorCode = "INVALID_TITLE"
+	BadPostBody    postErrorCode = "INVALID_BODY"
+	NoSuchPost     postErrorCode = "NO_SUCH_POST"
+
+	maxPostTitleLen int = 120
+)
+
 func checkError(err error) {
 	if err != nil {
 		LogError.Print(err)
 	}
 }
 
-func respond(w http.ResponseWriter, code int) {
-	w.WriteHeader(code)
-	_, err := w.Write([]byte{})
-	checkError(err)
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithBody(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
@@ -38,32 +47,165 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
+	var response Response
+
 	var post models.Post
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
-		respondWithJSON(w, http.StatusBadRequest, "Bad post body")
+		response.Error = BadPostBody
+		respondWithBody(w, http.StatusBadRequest, response)
 		return
 	}
 
-	LogInfo.Print("Got new post creation job\nPost:")
-	LogInfo.Print(post)
+	if len(post.Title) > maxPostTitleLen {
+		response.Error = InvalidTitle
+		respondWithBody(w, http.StatusBadRequest, response)
+		return
+	}
 
-	_, err = Db.Query("insert into posts(title, content) values($1, $2);",
-		post.Title, post.Content)
+	LogInfo.Printf("Got new post creation job. New post: %v", post)
 
+	var createdPost models.Post
+
+	err = Db.QueryRow("insert into posts(title, content) values($1, $2) RETURNING *;",
+		post.Title, post.Content).Scan(&createdPost.ID, &createdPost.Title, &createdPost.Date, &createdPost.Content)
 	if err != nil {
-		respondWithJSON(w, http.StatusInternalServerError, err)
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
 		return
 	}
-	respond(w, http.StatusCreated)
+
+	LogInfo.Printf("Created new post: %v", createdPost)
+
+	response.Body = createdPost
+	respondWithBody(w, http.StatusCreated, response)
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+	var response Response
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var post models.Post
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		response.Error = BadPostBody
+		respondWithBody(w, http.StatusBadRequest, response)
+		return
+	}
+
+	if len(post.Title) > maxPostTitleLen {
+		response.Error = InvalidTitle
+		respondWithBody(w, http.StatusBadRequest, response)
+		return
+	}
+
+	LogInfo.Printf("Got new post update job. New post: %v", post)
+
+	err = Db.QueryRow("select from posts where id = $1", id).Scan()
+	if err != nil {
+		if err == sql.ErrNoRows {
+			response.Error = NoSuchPost
+			respondWithBody(w, http.StatusNotFound, response)
+			return
+		}
+
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	var updatedPost models.Post
+	err = Db.QueryRow("UPDATE posts SET title = $1, content = $2 WHERE id = $3 RETURNING *;",
+		post.Title, post.Content, id).Scan(&updatedPost.ID, &updatedPost.Title, &updatedPost.Date, &updatedPost.Content)
+	if err != nil {
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	LogInfo.Printf("Updated post: %v", updatedPost)
+
+	response.Body = updatedPost
+	respondWithBody(w, http.StatusOK, response)
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+	var response Response
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	LogInfo.Printf("Got new post deletion job. Post id: %s", id)
+
+	err := Db.QueryRow("select from posts where id = $1", id).Scan()
+	if err != nil {
+		if err == sql.ErrNoRows {
+			response.Error = NoSuchPost
+			respondWithBody(w, http.StatusNotFound, response)
+			return
+		}
+
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	_, err = Db.Exec("DELETE FROM posts WHERE id = $1;", id)
+	if err != nil {
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	LogInfo.Printf("Post with id %s deleted", id)
+
+	respondWithBody(w, http.StatusOK, response)
+}
+
+func GetCertainPost(w http.ResponseWriter, r *http.Request) {
+	var response Response
+
+	var post models.Post
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	err := Db.QueryRow("select * from posts where id = $1", id).Scan(
+		&post.ID, &post.Title, &post.Date, &post.Content)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			response.Error = NoSuchPost
+			respondWithBody(w, http.StatusNotFound, response)
+			return
+		}
+
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	response.Body = post
+	respondWithBody(w, 200, response)
 }
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
+	var response Response
+
 	var posts []models.Post
 
 	rows, err := Db.Query("select * from posts")
 	if err != nil {
-		respondWithJSON(w, http.StatusInternalServerError, err)
+		response.Error = TechnicalError
+		LogError.Print(err)
+		respondWithBody(w, http.StatusInternalServerError, response)
 		return
 	}
 
@@ -71,43 +213,14 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		var currentPost models.Post
 		err = rows.Scan(&currentPost.ID, &currentPost.Title, &currentPost.Date, &currentPost.Content)
 		if err != nil {
-			respondWithJSON(w, http.StatusInternalServerError, err)
+			response.Error = TechnicalError
+			LogError.Print(err)
+			respondWithBody(w, http.StatusInternalServerError, response)
 			return
 		}
 		posts = append(posts, currentPost)
 	}
 
-	respondWithJSON(w, 200, posts)
-}
-
-func GetCertainPost(w http.ResponseWriter, r *http.Request) {
-	var post models.Post
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	row := Db.QueryRow("select * from posts where id = $1", id)
-
-	err := row.Scan(&post.ID, &post.Title, &post.Date, &post.Content)
-	if err != nil {
-		respondWithJSON(w, http.StatusInternalServerError, err)
-		return
-	}
-	respondWithJSON(w, 200, post)
-}
-
-func UpdatePost(w http.ResponseWriter, r *http.Request) {
-	var post models.Post
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	row := Db.QueryRow("select * from posts where id = $1", id)
-
-	err := row.Scan(&post.ID, &post.Title, &post.Date, &post.Content)
-	if err != nil {
-		respondWithJSON(w, http.StatusInternalServerError, err)
-		return
-	}
-	respondWithJSON(w, 200, post)
+	response.Body = posts
+	respondWithBody(w, 200, response)
 }
