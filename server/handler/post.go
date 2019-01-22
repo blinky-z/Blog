672891daa36f3ae31d66c796a6d223375aeace13
blinky-z <dm.blink19@gmail.com>
2019-01-22 13:05:42 +0300

    Add more user input validating to handlers

--- server/handler/post.go
+++ server/handler/post.go
@@ -38,8 +38,10 @@ const (
 	InvalidTitle PostErrorCode = "INVALID_TITLE"
 	// InvalidID - incorrect user input - invalid id of post
 	InvalidID PostErrorCode = "INVALID_ID"
-	// BadPostBody - incorrect user input - invalid content of posts
-	BadPostBody PostErrorCode = "INVALID_BODY"
+	// InvalidContent - incorrect user input - invalid content of post
+	InvalidContent PostErrorCode = "INVALID_CONTENT"
+	// BadRequestBody - incorrect user post - invalid json post
+	BadRequestBody PostErrorCode = "BAD_BODY"
 	// NoSuchPost - incorrect user input - requested post does not exist in database
 	NoSuchPost PostErrorCode = "NO_SUCH_POST"
 	// NoError - no error occurred while handling request
@@ -48,30 +50,33 @@ const (
 	maxPostTitleLen int = 120
 )
 
-func checkError(err error) {
+func respondWithBody(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	_, err := w.Write(response)
 	if err != nil {
 		LogError.Print(err)
 	}
-}
-
-func respondWithBody(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	_, err := w.Write(response)
-	checkError(err)
 }
 
 func validateUserPost(r *http.Request) (post models.Post, validateError PostErrorCode) {
 	validateError = NoError
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
-		validateError = BadPostBody
+		validateError = BadRequestBody
 		return
 	}
 
-	if len(post.Title) > maxPostTitleLen {
+	if len(post.Title) > maxPostTitleLen || len(post.Title) == 0 {
 		validateError = InvalidTitle
+		return
+	}
+
+	if len(post.Content) == 0 {
+		validateError = InvalidContent
 		return
 	}
 
@@ -203,8 +208,14 @@ func GetCertainPost(w http.ResponseWriter, r *http.Request) {
 
 	var post models.Post
 
-	vars := mux.Vars(r)
-	id := vars["id"]
+	id, validateIDError := validateUserID(r)
+	if validateIDError != NoError {
+		response.Error = validateIDError
+		respondWithBody(w, http.StatusBadRequest, response)
+		return
+	}
+
+	LogInfo.Printf("Got new post get job. Post id: %s", id)
 
 	err := Db.QueryRow("select id, title, date, content from posts where id = $1", id).Scan(
 		&post.ID, &post.Title, &post.Date, &post.Content)
@@ -220,6 +231,8 @@ func GetCertainPost(w http.ResponseWriter, r *http.Request) {
 		respondWithBody(w, http.StatusInternalServerError, response)
 		return
 	}
+
+	LogInfo.Printf("Post with id %s arrived from database", id)
 
 	response.Body = post
 	respondWithBody(w, 200, response)
