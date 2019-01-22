c1f899e15c143d3b126fa89a92aa6de2ab01e124
blinky-z <dm.blink19@gmail.com>
2019-01-22 22:40:41 +0300

    Update semantic of GetPosts func, more input validating in handlers
    Now GetPosts return range of posts but not all posts
    Add more input validating in handlers

--- server/handler/post.go
+++ server/handler/post.go
@@ -28,7 +28,7 @@ type Response struct {
 	Body  interface{}   `json:"body"`
 }
 
-// represents error occurred while handling request
+// PostErrorCode - represents error occurred while handling request
 type PostErrorCode string
 
 const (
@@ -44,6 +44,8 @@ const (
 	BadRequestBody PostErrorCode = "BAD_BODY"
 	// NoSuchPost - incorrect user input - requested post does not exist in database
 	NoSuchPost PostErrorCode = "NO_SUCH_POST"
+	// InvalidRange - user inputs invalid range of posts to get from database
+	InvalidRange PostErrorCode = "INVALID_POSTS_RANGE"
 	// NoError - no error occurred while handling request
 	NoError PostErrorCode = ""
 
@@ -60,6 +62,40 @@ func respondWithBody(w http.ResponseWriter, code int, payload interface{}) {
 	if err != nil {
 		LogError.Print(err)
 	}
+}
+
+func validateUserGetPostsParams(r *http.Request) (params map[string]int, validateError PostErrorCode) {
+	params = make(map[string]int)
+	validateError = NoError
+
+	var page int
+	var postsPerPage int
+	var err error
+
+	if len(r.FormValue("page")) == 0 {
+		validateError = InvalidRange
+		return
+	}
+
+	if page, err = strconv.Atoi(r.FormValue("page")); err != nil || page < 0 {
+		validateError = InvalidRange
+		return
+	}
+
+	if len(r.FormValue("posts-per-page")) == 0 {
+		postsPerPage = 10
+	} else {
+		if postsPerPage, err = strconv.Atoi(r.FormValue("posts-per-page")); err != nil || postsPerPage < 0 ||
+			postsPerPage > 40 {
+			validateError = InvalidRange
+			return
+		}
+	}
+
+	params["page"] = page
+	params["posts-per-page"] = postsPerPage
+
+	return
 }
 
 func validateUserPost(r *http.Request) (post models.Post, validateError PostErrorCode) {
@@ -111,8 +147,10 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	var createdPost models.Post
 
-	err := Db.QueryRow("insert into posts(title, content) values($1, $2) RETURNING id, title, date, content;",
+	_, _ = Db.Exec("BEGIN TRANSACTION")
+	err := Db.QueryRow("insert into posts(title, content) values($1, $2) RETURNING id, title, date, content",
 		post.Title, post.Content).Scan(&createdPost.ID, &createdPost.Title, &createdPost.Date, &createdPost.Content)
+	_, _ = Db.Exec("END TRANSACTION")
 	if err != nil {
 		response.Error = TechnicalError
 		LogError.Print(err)
@@ -161,8 +199,11 @@ func UpdatePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var updatedPost models.Post
-	err = Db.QueryRow("UPDATE posts SET title = $1, content = $2 WHERE id = $3 RETURNING id, title, date, content;",
+
+	_, _ = Db.Exec("BEGIN TRANSACTION")
+	err = Db.QueryRow("UPDATE posts SET title = $1, content = $2 WHERE id = $3 RETURNING id, title, date, content",
 		post.Title, post.Content, id).Scan(&updatedPost.ID, &updatedPost.Title, &updatedPost.Date, &updatedPost.Content)
+	_, _ = Db.Exec("END TRANSACTION")
 	if err != nil {
 		response.Error = TechnicalError
 		LogError.Print(err)
@@ -189,7 +230,9 @@ func DeletePost(w http.ResponseWriter, r *http.Request) {
 
 	LogInfo.Printf("Got new post deletion job. Post id: %s", id)
 
-	_, err := Db.Exec("DELETE FROM posts WHERE id = $1;", id)
+	_, _ = Db.Exec("BEGIN TRANSACTION")
+	_, err := Db.Exec("DELETE FROM posts WHERE id = $1", id)
+	_, _ = Db.Exec("END TRANSACTION")
 	if err != nil {
 		response.Error = TechnicalError
 		LogError.Print(err)
@@ -215,7 +258,7 @@ func GetCertainPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	LogInfo.Printf("Got new post get job. Post id: %s", id)
+	LogInfo.Printf("Got new get certain post job. Post id: %s", id)
 
 	err := Db.QueryRow("select id, title, date, content from posts where id = $1", id).Scan(
 		&post.ID, &post.Title, &post.Date, &post.Content)
@@ -238,13 +281,26 @@ func GetCertainPost(w http.ResponseWriter, r *http.Request) {
 	respondWithBody(w, 200, response)
 }
 
-// GetPosts - get all posts from database http handler
+// GetPosts - get one page of posts from database http handler
 func GetPosts(w http.ResponseWriter, r *http.Request) {
 	var response Response
 
+	params, validateError := validateUserGetPostsParams(r)
+	if validateError != NoError {
+		response.Error = validateError
+		respondWithBody(w, http.StatusBadRequest, response)
+		return
+	}
+
+	page := params["page"]
+	postsPerPage := params["posts-per-page"]
+
 	var posts []models.Post
 
-	rows, err := Db.Query("select id, title, date, content from posts")
+	LogInfo.Printf("Got new get range of posts job. Page: %d. Posts per page: %d", page, postsPerPage)
+
+	rows, err := Db.Query("select id, title, date, content from posts order by id DESC offset $1 limit $2",
+		page*postsPerPage, postsPerPage)
 	if err != nil {
 		response.Error = TechnicalError
 		LogError.Print(err)
@@ -263,6 +319,8 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		}
 		posts = append(posts, currentPost)
 	}
+
+	LogInfo.Print("Posts arrived from database")
 
 	response.Body = posts
 	respondWithBody(w, 200, response)
