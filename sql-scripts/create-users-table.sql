dc7995498d517acf6772467c94c369eb9aa5efa2
blinky-z <dm.blink19@gmail.com>
2019-01-27 16:55:00 +0300

    Update user table creation sql script

--- sql-scripts/create-users-table.sql
+++ sql-scripts/create-users-table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Users
+CREATE TABLE if not exists Users
 (
   LOGIN    CHARACTER VARYING(36)  not null,
   EMAIL    CHARACTER VARYING(255) not null,
   PASSWORD CHARACTER VARYING(255) not null
 );
 
-CREATE UNIQUE INDEX login ON users (LOGIN);
-CREATE UNIQUE INDEX login ON users (EMAIL);
+CREATE UNIQUE INDEX if not exists loginIndex ON users (LOGIN);
+CREATE UNIQUE INDEX if not exists emailIndex ON users (EMAIL);
