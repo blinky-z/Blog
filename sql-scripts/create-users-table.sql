7f54227ff611078fa35aed4e4eaad633db330caa
blinky-z <dm.blink19@gmail.com>
2019-02-04 15:21:34 +0300

    Update users table creation sql script: rename field 'LOGIN' on 'USERNAME'

--- sql-scripts/create-users-table.sql
+++ sql-scripts/create-users-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE if not exists Users
 (
-  LOGIN    CHARACTER VARYING(36)  not null,
+  USERNAME    CHARACTER VARYING(36)  not null,
   EMAIL    CHARACTER VARYING(255) not null,
   PASSWORD CHARACTER VARYING(255) not null
 );
 
-CREATE UNIQUE INDEX if not exists loginIndex ON users (LOGIN);
+CREATE UNIQUE INDEX if not exists loginIndex ON users (USERNAME);
 CREATE UNIQUE INDEX if not exists emailIndex ON users (EMAIL);
