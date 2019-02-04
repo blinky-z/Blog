a071f1d6f18d065f1480e01fad8005ec24e363d9
blinky-z <dm.blink19@gmail.com>
2019-02-04 15:18:08 +0300

    Create additional 'LoginRequest' and 'RegistrationRequest' for storing user's credentials

--- models/user.go
+++ models/user.go
@@ -3,9 +3,23 @@ package models
 // UserRole - represents user role
 type UserRole string
 
-// User - represents user credentials
+// User - represents user without password. Use it to work with user when you don't need secret information as password
 type User struct {
-	Login    string `json:"login"`
+	Username string
+	Email    string
+	Role     UserRole
+}
+
+// LoginRequest - represents credentials that user inputs on login page.
+type LoginRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// RegistrationRequest - represents credentials that user inputs on registration page.
+type RegistrationRequest struct {
+	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
