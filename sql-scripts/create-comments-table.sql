7ac3cb51430491dc7cebd0e3f10d91c5f247ebe3
blinky-z <dm.blink19@gmail.com>
2019-02-06 02:51:39 +0300

    Add comments table creation sql script

---
+++ sql-scripts/create-comments-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE if not exists Comments
+(
+  ID        SERIAL PRIMARY KEY,
+  POST_ID   INTEGER                 not null,
+  PARENT_ID INTEGER,
+  AUTHOR    CHARACTER VARYING(36)   not null,
+  DATE      TIMESTAMPTZ DEFAULT NOW(),
+  CONTENT   CHARACTER VARYING(2048) not null,
+  DELETED   BOOLEAN     DEFAULT FALSE
+);
+
+CREATE UNIQUE INDEX if not exists idIndex ON Comments (ID);
+CREATE INDEX if not exists postIdIndex ON Comments (POST_ID);
