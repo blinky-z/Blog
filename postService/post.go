326779dec9b4f22831e708870e0605dfd3bd96c1
blinky-z <dm.blink19@gmail.com>
2019-02-06 02:47:57 +0300

    Exclude db post fields into constants

--- postService/post.go
+++ postService/post.go
@@ -6,6 +6,13 @@ import (
 	"github.com/blinky-z/Blog/models"
 )
 
+const (
+	// DbPostInputFields - fields that should be filled while inserting new post
+	DbPostInputFields = "title, content, metadata"
+	// DbPostFields - all post fields
+	DbPostFields = "id, title, date, content, metadata"
+)
+
 // GetCertainPost - return post as models.Post. Used for sending post back to user using http with following client-side
 // rendering and also for server-side rendering of /posts/{id} page
 func GetCertainPost(env *models.Env, id string) (models.Post, error) {
@@ -16,7 +23,7 @@ func GetCertainPost(env *models.Env, id string) (models.Post, error) {
 	env.LogInfo.Printf("Getting post with ID %s from database", id)
 
 	var metadataAsJSONString string
-	if err := env.Db.QueryRow("select id, title, date, content, metadata from posts where id = $1", id).
+	if err := env.Db.QueryRow("select "+DbPostFields+" from posts where id = $1", id).
 		Scan(&post.ID, &post.Title, &post.Date, &post.Content, &metadataAsJSONString); err != nil {
 		if err == sql.ErrNoRows {
 			env.LogInfo.Printf("Can not GET post with ID %s : post does not exist", id)
@@ -44,7 +51,7 @@ func GetPosts(env *models.Env, page, postsPerPage int) ([]models.Post, error) {
 	env.LogInfo.Printf("Getting Range of Posts with following params: (page: %d, posts per page: %d) from database",
 		page, postsPerPage)
 
-	rows, err := env.Db.Query("select id, title, date, content, metadata from posts order by id DESC offset $1 limit $2",
+	rows, err := env.Db.Query("select "+DbPostFields+" from posts order by id DESC offset $1 limit $2",
 		page*postsPerPage, postsPerPage)
 	if err != nil {
 		env.LogError.Print(err)
