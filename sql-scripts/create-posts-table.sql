3e3ae9a2ac64796d63698c4c01e1f50daa938b35
Blinky <dm.blink19@gmail.com>
2019-08-20 17:26:45 +0300

    Change 'posts' table
    1. Sync title len with code
    2. Add 'not null' constraint to md content

--- sql-scripts/create-posts-table.sql
+++ sql-scripts/create-posts-table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE if not exists posts
 (
     ID         SERIAL PRIMARY KEY,
-    TITLE      CHARACTER VARYING(120) not null,
+    TITLE      CHARACTER VARYING(200) not null,
     DATE       TIMESTAMPTZ DEFAULT NOW(),
     METADATA   text                   not null,
     SNIPPET    text                   not null,
     CONTENT    text                   not null,
-    CONTENT_MD text
+    CONTENT_MD text                   not null
 );
